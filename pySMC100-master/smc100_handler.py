1dee9cb145cf51cb9956fe9c32ec1ebf555aa223
sadra <sadra701@gmail.com>
2025-03-07 16:03:40 +0300

    works now

--- pySMC100-master/smc100_handler.py
+++ pySMC100-master/smc100_handler.py
@@ -1,11 +1,16 @@
 from pystages.smc100 import SMC100
 from pystages import Vector
 
-smc = SMC100(dev="/dev/ttyUSB1", addresses=[1])
+smc = SMC100(dev="/dev/ttyUSB0", addresses=[1])
+smc.enter_leave_disable_state(1, False)
+
 smc.home(wait=True)
+smc.move_to(Vector(10))
+
+
 print(smc.controller_address(1))
 print(smc.get_error_and_state(1).state)
 #smc.enter_configuration_state(2)
 
-smc.move_to(Vector(10))
+
 #smc.reset(1)
